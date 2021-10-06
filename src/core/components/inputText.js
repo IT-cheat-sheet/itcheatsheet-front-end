c5bbc349d5b3b70e722362dd2b838074395b2a36
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-06 22:12:38 +0700

    [EDIT] Use Design System in InputText

--- src/core/components/inputText.js
+++ src/core/components/inputText.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
-const InputText = ({label, labelColor, textColor, placeholder, placeholderColor}) => {
+const InputText = ({label, placeholder, page}) => {
+  const labelColor = page === 'sheet' ? 'text-violet-link' : 'text-blue-body';
+  const placeholderColor = page === 'sheet' ? 'placeholder-violet-bubbleHover' : 'placeholder-lightblue-lighter';
+  const ringHoverColor = "hover:text-" + (page === 'sheet' ? 'purple' : 'blue') + "-form";
+  const ringFocusColor = "focus:text-" + (page === 'sheet' ? 'purple' : 'blue') + "-formHover";
+
   return (
     <div>
-      <div className={`uppercase text-2xl mb-5 ${labelColor}`}>{label}</div>
-      <input type="text" placeholder={placeholder} className={`rounded-lg h-12 text-2xl py-2 px-4 focus:outline-none ${textColor} ${placeholderColor}`}/>
+      <div className={`uppercase body-base mb-5 ${labelColor}`}>{label}</div>
+      <input type="text" placeholder={placeholder} className={`rounded-lg h-12 body-base py-2 px-4 input text-gray-mailbox ${ringHoverColor} ${ringFocusColor} ${placeholderColor}`}/>
     </div>
   )
 } 
