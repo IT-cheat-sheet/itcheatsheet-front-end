dec99241b5316c441ad7249519c194cfa7de8efc
Pathinya <gift_01210@hotmail.com>
2021-10-06 15:47:37 +0700

    [EDIT] using design system in TextArea.js

--- src/core/components/textArea.js
+++ src/core/components/textArea.js
@@ -2,14 +2,14 @@ import React from 'react'
 
 export default function textArea(props) {
   return (
-    <div style={{ color: props.page === 'sheet' ? '#A565EF' : '#2F6ECB' }}>
+    <div className={`${props.page === 'sheet' ? 'text-violet-pill' : 'text-blue-body'}`}>
       <label className="uppercase text-2xl" for="textAreaContent">{props.textAreaLabel}</label>
       <br />
       <textarea 
         id="textAreaContent" 
         name="textAreaContent" 
         className={`resize-none w-3/5 h-60 mt-5 rounded-lg py-2 px-4
-        ${props.page === 'sheet' ? 'placeholder-purple-400' : 'placeholder-blue-400'}`} 
+        ${props.page === 'sheet' ? 'placeholder-violet-pill' : 'placeholder-blue-body'}`} 
         placeholder="Please text politely :)">  
       </textarea>
     </div>
