3031f792588459a8c2faf10e823693266b45b8e1
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-11 23:02:19 +0700

    [ADD] Base All Sheet Page w/ Temp Sheet

---
+++ src/pages/all_sheets/sheet_thumb.js
@@ -0,0 +1,22 @@
+import classNames from "classnames";
+import React, { useState } from "react";
+import { Document, Page, pdfjs } from 'react-pdf';
+import './sheet_thumb.css'
+
+export default function SheetThumb({ url, fileName}) {
+  const [isHover, setIsHover] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+  return (<div className="p-10 cursor-pointer" onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+    <div className={classNames("w-full rounded-lg overflow-hidden transition duration-200 transform",
+    {"scale-105" : isHover},
+    {"shadow-halo" : isLoaded})}>
+      <Document file={url} onLoadSuccess={() => setIsLoaded(true)}>
+        <Page pageNumber={1}/>
+      </Document>
+    </div>
+    <div className="body-base uppercase text-gray-header text-center mt-5">{fileName}</div>
+  </div>)
+}
