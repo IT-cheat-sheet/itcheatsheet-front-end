dc02a545959c85476de2b5b82cfcaa5e1e9afaf9
Pathinya <gift_01210@hotmail.com>
2021-10-15 17:04:50 +0700

    [EDIT] preview review page with fetch api (no test yet)

--- src/pages/preview-review/preview_review_page.js
+++ src/pages/preview-review/preview_review_page.js
@@ -1,37 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router';
 import Kebab from '../../core/components/kebab'
 import RecommendedBlock from './recommended_block'
+import { Link } from 'react-router-dom';
 
 export default function PreviewReview() {
+  const params = useParams();
+  const [reviewPost, setReviewPost] = useState(null);
+  const [reviewImage, setReviewImage] = useState([]);
+  const [isLoad, setIsload] = useState(false);
+  const [recommendedPost, setRecommendedPost] = useState([]);
+
+  const fetchReviewPost = async () => {
+    const res = await fetch(`http://localhost:3000/review/get/${params.id}`);
+    const data = await res.json();
+    setReviewPost(data);
+  }
+
+  const fetchReviewImage = async () => {
+    const res = await fetch(`http://localhost:3000/review/image/${params.id}`);
+    setReviewImage(res);
+  }
+
+  const fetchRecommendedPost = async () => {
+    const res = await fetch(`http://localhost:3000/review/random`);
+    const data = await res.json();
+    setRecommendedPost(data.data);
+    setIsload(true)
+  }
+
+  useEffect(() => {
+    fetchReviewPost();
+    fetchReviewImage();
+    fetchRecommendedPost();
+  }, [])
+
+  const RecommendedBlocks = recommendedPost.map((review) =>
+    <Link to={`/reviews/${review.reviewId}`}>
+      <RecommendedBlock
+        image={`http://localhost:3000/review/image/${review.reviewId}`}
+        reviewTitle={review.reviewTitle}
+        reviewContent={review.reviewContent}
+        reviewer={review.reviewer}
+        reviewId={review.reviewId} />
+    </Link>
+  )
+
   return (
-    <div>
-      <h1 className="uppercase header-secondary text-blue-body">thread name</h1>
-      <div className="flex my-2">
-        <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">post owner</p>
-        <p className="ml-2.5 text-blue-page">POST ID</p>
-      </div>
-      <div className="mt-5 body-base bg-lightblue-bg rounded-lg pb-12">
-        <div className="flex justify-end mr-6 pt-2">
-          <Kebab page="review" />
-        </div>
-        <p className="mx-14 text-blue-body">
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-          when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-          It has survived not only five centuries, but also the leap into electronic typesetting,
-          remaining essentially unchanged.
-          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-        </p>
-        <img src="sitMusic7-01.png" className="mt-5 w-4/6 mx-auto" />
-      </div>
-      <h4 className="uppercase body-base text-blue-body mt-12">recommended threads</h4>
-      <div className="grid grid-cols-2 gap-4 mt-5 mb-24">
-        <RecommendedBlock />
-        <RecommendedBlock />
-        <RecommendedBlock />
-        <RecommendedBlock />
-      </div>
-    </div>
+    <div className="mx-44">
+      {isLoad ?
+        <>
+          <h1 className="uppercase header-secondary text-blue-body" > {reviewPost.data.reviewTitle}</h1 >
+          <div className="flex my-2">
+            <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">{reviewPost.data.reviewer}</p>
+            <p className="ml-2.5 text-blue-page">{reviewPost.data.reviewId}</p>
+          </div>
+          <div className="mt-5 body-base bg-lightblue-bg rounded-lg pb-12">
+            <div className="flex justify-end mr-6 pt-2">
+              <Kebab page="review" />
+            </div>
+            <p className="mx-14 text-blue-body">
+              {reviewPost.data.reviewContent}
+            </p>
+            {
+              reviewImage ?
+              <img src={reviewImage} className="mt-5 w-4/6 mx-auto" alt="review" />
+              : <></>
+            }
+          </div>
+          <h4 className="uppercase body-base text-blue-body mt-12">recommended threads</h4>
+          <div className="grid grid-cols-2 gap-4 mt-5 mb-24">
+            <RecommendedBlocks />
+          </div>
+        </>
+        : <></>
+      }
+    </div >
+
   )
 }
--- src/pages/preview-review/recommended_block.js
+++ src/pages/preview-review/recommended_block.js
@@ -1,36 +1,26 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 
-export default function RecommendedBlock() {
-  const image = "sitMusic7-01.png"
+export default function RecommendedBlock(props) {
   return (
-    // <Link to="/">
       <div className="w-full grid grid-cols-4 text-blue-body bg-lightblue-bg rounded-lg h-44 cursor-pointer hover:shadow-xl">
-        {image ?
+        {props.image ?
           <div>
-            <img src={image} className="h-44 object-cover rounded-l-lg" />
+            <img src={props.image} className="h-44 object-cover rounded-l-lg" alt="review"/>
           </div>
           : <></>
         }
-        <div className={`pl-4 table mr-4 ${image === "" ? "col-span-4" : "col-span-3"}`}>
+        <div className={`pl-4 table mr-4 ${props.image === "" ? "col-span-4" : "col-span-3"}`}>
           <div className="table-cell align-middle">
-            <h5 className="body-base">Thread's name</h5>
+            <h5 className="body-base">{props.reviewTitle}</h5>
             <p className="line-clamp-3">
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-              Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-              when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-              It has survived not only five centuries, but also the leap into electronic typesetting,
-              remaining essentially unchanged.
-              It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-              and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              {props.reviewContent}
             </p>
             <div className="flex my-2 justify-end">
-              <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">post owner</p>
-              <p className="ml-2.5 text-blue-page">POST ID</p>
+              <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">{props.reviewer}</p>
+              <p className="ml-2.5 text-blue-page">{props.reviewId}</p>
             </div>
           </div>
         </div>
       </div>
-    // </Link>
   )
 }
