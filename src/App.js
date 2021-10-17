d816564423520c070e8bf573c496bb38693f5628
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-17 20:44:43 +0700

    [EDIT] Add Route for All Reviews and Sheets

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import HomePage from "./pages/home/home_page";
+import AllSheets from "./pages/all_sheets/all_sheets_page";
+import AllReviews from "./pages/all_reviews/all_reviews_page";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -12,8 +14,12 @@ function App() {
           <Route path="/admin/review/:id"></Route>
           <Route path="/admin/sheet/:id"></Route>
 
-          <Route path="/sheets"></Route>
-          <Route path="/reviews"></Route>
+          <Route path="/sheets">
+            <AllSheets />
+          </Route>
+          <Route path="/reviews">
+            <AllReviews />
+          </Route>
           <Route path="/admin"></Route>
 
           <Route path="/">
