bce3e3e87aaa4544e7e83b22b125527ce8877518
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-17 17:14:05 +0700

    [EDIT] Searchbox /w Update Functions

--- src/core/components/searchBox.js
+++ src/core/components/searchBox.js
@@ -2,12 +2,8 @@ import classNames from "classnames";
 import React, { useState } from "react";
 import Button from "./button";
 
-export default function SearchBox({ page, options }) {
+export default function SearchBox({ page, options, onFilter, onSearch }) {
   const [toggle, setToggle] = useState(false);
-
-  const updateValue = () => {
-
-  }
 
   return (
     <div className={classNames("grid grid-cols-12 gap-5 px-6 py-4 rounded-lg body-base",
@@ -18,22 +14,27 @@ export default function SearchBox({ page, options }) {
         <Button color={page === "review" ? "blue" : "purple"} size="sm" children="SORT BY" />
         {toggle ?
         <div className={`shadow-halo absolute top-14 rounded-lg z-20`}>
+          <div
+            onClick={() => {
+              onFilter('');
+              setToggle(false);
+            }}
+            className={`bg-lightblue-lighter w-48 py-3 px-4 body-base cursor-pointer hover:bg-blue-form text-blue-body hover:text-white rounded-t-lg`}>None</div>
           {options.map((option, index) => (
             <div
             onClick={() => {
-              updateValue(option);
+              onFilter(option);
               setToggle(false);
             }}
             className={`bg-lightblue-lighter w-48 py-3 px-4 body-base cursor-pointer hover:bg-blue-form text-blue-body hover:text-white
             ${index === options.length - 1 ? 'rounded-b-lg' : ''}
-            ${index === 0 ? 'rounded-t-lg' : ''}
             `}>{option}</div>
           ))}
         </div>
       : <></>}
       </div>
-      <div className="col-span-8"><input className="w-full h-full rounded-lg px-4 focus:outline-none" /></div>
-      <div className="col-span-2"><Button color={page === "review" ? "blue" : "purple"} size="sm" children={<i className="fas fa-search" />} /></div>
+      <div className="col-span-8"><input className="w-full h-full rounded-lg px-4 focus:outline-none" onChange={(e) => onSearch(e.target.value)} /></div>
+      <div className="col-span-2" onClick={onSearch}><Button color={page === "review" ? "blue" : "purple"} size="sm" children={<i className="fas fa-search" />} /></div>
     </div>
   )
 }
