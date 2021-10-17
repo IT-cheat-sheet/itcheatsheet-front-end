d1e19d33108639b6101b3ded4e0ed4d19ae6be4a
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-17 17:10:48 +0700

    [EDIT] All Reviews Page w/ Fetch API

--- src/pages/all_reviews/review_thumb.js
+++ src/pages/all_reviews/review_thumb.js
@@ -7,26 +7,34 @@ export default function ReviewThumb({ review }) {
   useEffect(() => {
     (async function() {
       const res = await fetch(`http://localhost:3000/review/image/${review.reviewId}`);
-      const data = await res.data;
+      const data = await res.blob();
       
       if(res.status === 200){
-        setImage(data);
+        if (data.type.includes("image")) {
+          var reader = new FileReader();
+          reader.onload = (e) => {
+            setImage(e.target.result);
+          };
+          reader.readAsDataURL(data);
+        }
+      } else {
+        setImage(null);
       }
     })();
   }, [review])
 
   return (
-    <div className="p-3">
+    <div className="p-3 h-72">
       <div
       onMouseEnter={() => setIsHover(true)}
       onMouseLeave={() => setIsHover(false)}
       className={`relative grid grid-cols-12 h-full rounded-lg shadow-halo transition duration-200 transform cursor-pointer ${isHover ? "bg-blue-button text-white" : "bg-lightblue-bg text-blue-body"}`}>
         {image ? <div className="col-span-5"><img className="object-cover w-full h-full rounded-l-lg" src={image} alt="Not Found"/></div> : ""}
-        <div className={`${review.reviewImage ? "col-span-7 py-14" : "col-span-12 py-8"} px-9`}>
-          <div className="w-full body-l font-bold mb-3">
+        <div className={`${image ? "col-span-7" : "col-span-12"} flex flex-col justify-center px-9`}>
+          <div className={`w-full body-l font-bold mb-3 truncate`}>
             {review.reviewTitle}
           </div>
-          <div className={`body-sm ${review.reviewImage ? "line-clamp-4" : "line-clamp-6"}`}>
+          <div className={`body-sm ${image ? "line-clamp-4" : "line-clamp-6"}`}>
             {review.reviewContent}
           </div>
         </div>
