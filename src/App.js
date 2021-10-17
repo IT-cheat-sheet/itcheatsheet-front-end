eef4bebd5afd37ef1284efdd52e60d56294649e4
pizza <npwchr.work@gmail.com>
2021-10-17 16:37:27 +0700

    create route

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import HomePage from './pages/home/home_page';
+import "./App.css";
+import HomePage from "./pages/home/home_page";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-        <HomePage/>
+      <Router>
+        <Switch>
+          <Route path="/sheets/:id"></Route>
+          <Route path="/reviews/:id"></Route>
+          <Route path="/admin/review/:id"></Route>
+          <Route path="/admin/sheet/:id"></Route>
+
+          <Route path="/sheets"></Route>
+          <Route path="/reviews"></Route>
+          <Route path="/admin"></Route>
+
+          <Route path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
