607c45fc25195f66073ea8a310f70535eb236815
Pathinya <gift_01210@hotmail.com>
2021-10-18 18:54:07 +0700

    [EDIT] Add route to specific sheet and review

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import "./App.css";
 import HomePage from "./pages/home/home_page";
 import AllSheets from "./pages/all_sheets/all_sheets_page";
 import AllReviews from "./pages/all_reviews/all_reviews_page";
+import PreviewSheet from "./pages/preview-sheet/preview_sheet_page";
+import PreviewReview from "./pages/preview-review/preview_review_page"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
@@ -9,8 +11,12 @@ function App() {
     <div className="App">
       <Router>
         <Switch>
-          <Route path="/sheets/:id"></Route>
-          <Route path="/reviews/:id"></Route>
+          <Route path="/sheets/:id">
+            <PreviewSheet />
+          </Route>
+          <Route path="/reviews/:id">
+            <PreviewReview />
+          </Route>
           <Route path="/admin/review/:id"></Route>
           <Route path="/admin/sheet/:id"></Route>
 
