58bb9b1f8d19e0cb2cb6c858dbef7fa32368a41b
Pathinya <gift_01210@hotmail.com>
2021-10-18 15:52:08 +0700

    [EDIT] fetch review post

--- src/pages/preview-review/preview_review_page.js
+++ src/pages/preview-review/preview_review_page.js
@@ -6,74 +6,88 @@ import { Link } from 'react-router-dom';
 
 export default function PreviewReview() {
   const params = useParams();
-  const [reviewPost, setReviewPost] = useState(null);
+  const [reviewPost, setReviewPost] = useState([]);
   const [reviewImage, setReviewImage] = useState([]);
-  const [isLoad, setIsload] = useState(false);
-  const [recommendedPost, setRecommendedPost] = useState([]);
-
-  const fetchReviewPost = async () => {
-    const res = await fetch(`http://localhost:3000/review/get/${params.id}`);
-    const data = await res.json();
-    setReviewPost(data);
-  }
-
-  const fetchReviewImage = async () => {
-    const res = await fetch(`http://localhost:3000/review/image/${params.id}`);
-    setReviewImage(res);
-  }
-
-  const fetchRecommendedPost = async () => {
-    const res = await fetch(`http://localhost:3000/review/random`);
-    const data = await res.json();
-    setRecommendedPost(data.data);
-    setIsload(true)
-  }
+  const [isLoad, setIsLoad] = useState(false);
 
   useEffect(() => {
+    async function fetchReviewPost() {
+      const res = await fetch(`http://localhost:3000/review/get/${params.id}`);
+      const data = await res.json();
+      setReviewPost(data);
+    };
+    async function fetchReviewImage() {
+      const res = await fetch(`http://localhost:3000/review/image/${params.id}`);
+      const data = await res.blob();
+
+      if (res.status === 200) {
+        if (data.type.includes("image")) {
+          var reader = new FileReader();
+          reader.onload = (e) => {
+            setReviewImage(e.target.result);
+          };
+          reader.readAsDataURL(data);
+        }
+      } else {
+        setReviewImage(null);
+      }
+      setIsLoad(true);
+    };
+
     fetchReviewPost();
     fetchReviewImage();
-    fetchRecommendedPost();
-  }, [])
+  }, [params.id])
 
-  const RecommendedBlocks = recommendedPost.map((review) =>
-    <Link to={`/reviews/${review.reviewId}`}>
-      <RecommendedBlock
-        image={`http://localhost:3000/review/image/${review.reviewId}`}
-        reviewTitle={review.reviewTitle}
-        reviewContent={review.reviewContent}
-        reviewer={review.reviewer}
-        reviewId={review.reviewId} />
-    </Link>
-  )
+  const RecommendedBlocks = () => {
+    const [reviews, setReviews] = useState([]);
+
+    useEffect(() => {
+      async function fetchRecommendedPost() {
+        const res = await fetch(`http://localhost:3000/review/random`);
+        const data = await res.json();
+        setReviews(data.data.map((review,index) =>
+        (
+          <Link to={`/reviews/${review.reviewId}`} key={index}>
+            <RecommendedBlock
+              review={review} />
+          </Link>
+        )));
+      }
+      fetchRecommendedPost();
+    }, [])
+
+    return reviews;
+  }
 
   return (
     <div className="mx-44">
-      {isLoad ?
-        <>
-          <h1 className="uppercase header-secondary text-blue-body" > {reviewPost.data.reviewTitle}</h1 >
-          <div className="flex my-2">
-            <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">{reviewPost.data.reviewer}</p>
-            <p className="ml-2.5 text-blue-page">{reviewPost.data.reviewId}</p>
-          </div>
-          <div className="mt-5 body-base bg-lightblue-bg rounded-lg pb-12">
-            <div className="flex justify-end mr-6 pt-2">
-              <Kebab page="review" />
+      {
+        isLoad ?
+          <>
+            <h1 className="uppercase header-secondary text-blue-body" > {reviewPost.data.reviewTitle}</h1 >
+            <div className="flex my-2">
+              <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">{reviewPost.data.reviewer}</p>
+              <p className="ml-2.5 text-blue-page">{reviewPost.data.reviewId}</p>
             </div>
-            <p className="mx-14 text-blue-body">
-              {reviewPost.data.reviewContent}
-            </p>
-            {
-              reviewImage ?
-              <img src={reviewImage} className="mt-5 w-4/6 mx-auto" alt="review" />
-              : <></>
-            }
-          </div>
-          <h4 className="uppercase body-base text-blue-body mt-12">recommended threads</h4>
-          <div className="grid grid-cols-2 gap-4 mt-5 mb-24">
-            <RecommendedBlocks />
-          </div>
-        </>
-        : <></>
+            <div className="mt-5 body-base bg-lightblue-bg rounded-lg pb-12">
+              <div className="flex justify-end mr-6 pt-2">
+                <Kebab page="review" />
+              </div>
+              <p className="mx-14 text-blue-body">
+                {reviewPost.data.reviewContent}
+              </p>
+              {
+                reviewImage ?
+                  <img src={reviewImage} className="mt-5 w-4/6 mx-auto" alt="review" />
+                  : <></>
+              }
+            </div>
+            <h4 className="uppercase body-base text-blue-body mt-12">recommended threads</h4>
+            <div className="grid grid-cols-2 gap-4 mt-5 mb-24">
+              <RecommendedBlocks />
+            </div>
+          </>
+          : <></>
       }
     </div >
 
--- src/pages/preview-review/recommended_block.js
+++ src/pages/preview-review/recommended_block.js
@@ -1,23 +1,44 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-export default function RecommendedBlock(props) {
+export default function RecommendedBlock({ review }) {
+  const [image, setImage] = useState(null);
+
+  useEffect(() => {
+    (async function() {
+      const res = await fetch(`http://localhost:3000/review/image/${review.reviewId}`);
+      const data = await res.blob();
+      
+      if(res.status === 200){
+        if (data.type.includes("image")) {
+          var reader = new FileReader();
+          reader.onload = (e) => {
+            setImage(e.target.result);
+          };
+          reader.readAsDataURL(data);
+        }
+      } else {
+        setImage(null);
+      }
+    })();
+  }, [review])
+
   return (
       <div className="w-full grid grid-cols-4 text-blue-body bg-lightblue-bg rounded-lg h-44 cursor-pointer hover:shadow-xl">
-        {props.image ?
+        {image ?
           <div>
-            <img src={props.image} className="h-44 object-cover rounded-l-lg" alt="review"/>
+            <img src={image} className="h-44 object-cover rounded-l-lg" alt="review"/>
           </div>
           : <></>
         }
-        <div className={`pl-4 table mr-4 ${props.image === "" ? "col-span-4" : "col-span-3"}`}>
+        <div className={`pl-4 table mr-4 ${image ? "col-span-3" : "col-span-4"}`}>
           <div className="table-cell align-middle">
-            <h5 className="body-base">{props.reviewTitle}</h5>
+            <h5 className="body-base">{review.reviewTitle}</h5>
             <p className="line-clamp-3">
-              {props.reviewContent}
+              {review.reviewContent}
             </p>
             <div className="flex my-2 justify-end">
-              <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">{props.reviewer}</p>
-              <p className="ml-2.5 text-blue-page">{props.reviewId}</p>
+              <p className="uppercase px-6 rounded-3xl bg-blue-form text-white">{review.reviewer}</p>
+              <p className="ml-2.5 text-blue-page">{review.reviewId}</p>
             </div>
           </div>
         </div>
