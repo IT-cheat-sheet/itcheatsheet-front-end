5c983fb700a89fe6d19d0bba106e9d49bac5c054
Poonpipat Suvanarawat <poonpipat.exp@mail.kmutt.ac.th>
2021-10-19 17:40:16 +0700

    merge local

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/home/home_page';
+import Navbar from "./core/components/navbar";
+import Footer from "./core/components/footer";
 function App() {
   return (
-    <div className="App">
+    <div className="App ">
+        <Navbar/>
         <HomePage/>
+        <Footer/>
     </div>
   );
 }
