138b4d8cc0acac4d7457144fbcd285a4be39d48c
Pathinya <gift_01210@hotmail.com>
2021-10-19 22:02:14 +0700

    [EDIT] Responsive Preview Sheet

--- src/pages/preview-sheet/preview_sheet_page.js
+++ src/pages/preview-sheet/preview_sheet_page.js
@@ -3,6 +3,8 @@ import Kebab from '../../core/components/kebab'
 import Button from '../../core/components/button'
 import { Document, Page, pdfjs } from 'react-pdf';
 import { useParams } from 'react-router';
+import Navbar from '../../core/components/navbar'
+import Footer from '../../core/components/footer'
 
 export default function PreviewSheet() {
   pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
@@ -39,8 +41,22 @@ export default function PreviewSheet() {
 
   return (
     <div>
+      <Navbar />
       {isLoad ?
-        <div className={`grid gap-7 pl-44 ${file ? "grid-cols-3" : ""}`}>
+        <div className={`px-4 pb-12 md:px-16 lg:px-20 xl:px-44 bg-violet-bg pt-20 lg:pt-0 md:bg-white md:grid md:gap-7 ${file ? "md:grid-cols-3" : ""}`}>
+          <div className="md:hidden">
+            <p className="text-xxs text-purple-hover">{sheet.summaryPost.semester.semester}</p>
+            <div className="mt-2">
+              <h1 className="text-2xl font-bold text-violet-header mr-4">{sheet.summaryPost.summaryTitle}</h1>
+              <div className="float-right -mt-8">
+                <Kebab page="sheet" />
+              </div>
+            </div>
+            <div className="flex mt-1 mb-5">
+              <p className="px-2 text-xxs rounded-3xl bg-violet-pill text-white">{sheet.summaryPost.posterName}</p>
+              <p className="ml-4 text-purple-hover text-xxs">{sheet.summaryPost.summaryPostId}</p>
+            </div>
+          </div>
           {
             file ?
               <a href={`http://localhost:3000/summarypost/getFile/${params.id}`} target="_blank" rel="noreferrer">
@@ -54,21 +70,23 @@ export default function PreviewSheet() {
               </a>
               : <></>
           }
-          <div className="mt-16 col-span-2">
-            <div className="flex">
-              <h2 className="header-secondary text-violet-header uppercase">{sheet.summaryPost.summaryTitle}</h2>
-              <p className="ml-3 text-purple-hover mt-9">{sheet.summaryPost.semester.semester}</p>
+          <div className="mt-5 xl:mt-16 md:col-span-2">
+            <div className="hidden md:block">
+              <div className="flex">
+                <h2 className="header-secondary text-violet-header">{sheet.summaryPost.summaryTitle}</h2>
+                <p className="ml-3 text-purple-hover mt-9">{sheet.summaryPost.semester.semester}</p>
+              </div>
+              <div className="flex my-2">
+                <p className="px-6 rounded-3xl bg-violet-pill text-white">{sheet.summaryPost.posterName}</p>
+                <p className="ml-2.5 text-purple-hover">{sheet.summaryPost.summaryPostId}</p>
+              </div>
             </div>
-            <div className="flex my-2">
-              <p className="px-6 rounded-3xl bg-violet-pill text-white">{sheet.summaryPost.posterName}</p>
-              <p className="ml-2.5 text-purple-hover">{sheet.summaryPost.summaryPostId}</p>
-            </div>
-            <div className=" mr-44 mt-5 body-base">
-              <div className="bg-violet-bg pt-5 pb-12">
-                <div className="flex justify-end mr-6">
+            <div className="text-xs md:mt-5 md:text-2xl">
+              <div className="bg-violet-bg md:pt-5 pb-12">
+                <div className="hidden md:flex md:justify-end md:mr-6">
                   <Kebab page="sheet" />
                 </div>
-                <div className="mx-14">
+                <div className="md:mx-14">
                   <p className="text-violet-header">
                     {sheet.summaryPost.summaryContent}
                   </p>
@@ -85,7 +103,7 @@ export default function PreviewSheet() {
               </div>
               {
                 file ?
-                  <div className="w-1/4 text-center cursor-pointer mt-5 ml-auto text-white">
+                  <div className="md:w-1/4 md:mb-12 text-center cursor-pointer mt-5 ml-auto text-white">
                     <a href={file} download={sheet.summaryPost.summaryTitle}>
                       <Button color="purple" size="sm">Download</Button>
                     </a>
@@ -97,6 +115,7 @@ export default function PreviewSheet() {
         </div>
         : <></>
       }
+      <Footer />
     </div>
   )
 }
