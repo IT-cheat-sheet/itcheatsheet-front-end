84eed45bda86d9f36d2d8941e3ed6554a7693205
Pathinya <gift_01210@hotmail.com>
2021-10-21 14:03:24 +0700

    [EDIT] responsive kebab

--- src/core/components/kebab.js
+++ src/core/components/kebab.js
@@ -3,18 +3,18 @@ import React, { useState } from 'react'
 export default function Kebab({ page }) {
   const [toggle, setToggle] = useState(false);
   return (
-    <div className={`text-right relative w-64 ${page === 'sheet' ? 'text-violet-hover' : 'text-blue-body'}`}>
+    <div className={`text-right relative w-27 md:w-36 lg:w-64 ${page === 'sheet' ? 'text-violet-hover' : 'text-blue-body'}`}>
       <span className="material-icons cursor-pointer p-1" onClick={() => setToggle(!toggle)}>more_vert</span>
       {toggle
-        ? <div className={`uppercase w-60 text-center absolute top-0 right-7 text-xl rounded-lg ${page === 'sheet' ? 'bg-violet-bubble' : 'bg-lightblue-light'}`}>
-          <p className={`py-5 cursor-pointer rounded-t-lg flex justify-center items-center ${page === 'sheet' ? 'hover:bg-violet-bubbleHover' : 'hover:bg-lightblue-hover'}`} onClick={() => alert('share modal open')}>
+        ? <div className={`uppercase w-27 md:w-36 lg:w-60 text-center absolute top-0 right-7 text-xs md:text-sm lg:text-xl rounded-lg ${page === 'sheet' ? 'bg-violet-bubble' : 'bg-lightblue-lighter'}`}>
+          <p className={`py-2 lg:py-5 cursor-pointer rounded-t-lg flex justify-center items-center ${page === 'sheet' ? 'hover:bg-violet-bubbleHover' : 'hover:bg-lightblue-light'}`} onClick={() => alert('share modal open')}>
             share
-            <span className="material-icons ml-1.5">share</span>
+            <span className="material-icons ml-1.5 text-xs md:text-base lg:text-xl">share</span>
           </p>
-          <hr className={`opacity-100 ${page === 'sheet' ? 'border-violet-hover' : 'bg-lightblue-hover'}`} />
-          <p className={`py-5 cursor-pointer rounded-b-lg flex justify-center items-center ${page === 'sheet' ? 'hover:bg-violet-bubbleHover' : 'hover:bg-lightblue-hover'}`} onClick={() => alert('suggest modal open')}>
+          <hr className={`opacity-100 ${page === 'sheet' ? 'border-violet-hover' : 'bg-lightblue-light'}`} />
+          <p className={`py-2 lg:py-5 cursor-pointer rounded-b-lg flex justify-center items-center ${page === 'sheet' ? 'hover:bg-violet-bubbleHover' : 'hover:bg-lightblue-light'}`} onClick={() => alert('suggest modal open')}>
             suggestion
-            <span className="material-icons ml-1.5">edit</span>
+            <span className="material-icons ml-1.5 text-xs md:text-base lg:text-xl">edit</span>
           </p>
         </div>
         : <></>}
