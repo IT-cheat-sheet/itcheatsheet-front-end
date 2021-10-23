4c14371ce48ca2a2755ceffd9fa73de066f86d8f
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-24 00:13:55 +0700

    [EDIT] Add Route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import AllReviews from "./pages/all_reviews/all_reviews_page";
 import PreviewSheet from "./pages/preview-sheet/preview_sheet_page";
 import PreviewReview from "./pages/preview-review/preview_review_page"
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import AdminHome from "./pages/admin_home/admin_home_page";
 
 function App() {
   return (
@@ -17,8 +18,9 @@ function App() {
           <Route path="/reviews/:id">
             <PreviewReview />
           </Route>
-          <Route path="/admin/review/:id"></Route>
-          <Route path="/admin/sheet/:id"></Route>
+          <Route path="/admin/reviews/:id"></Route>
+          <Route path="/admin/sheets/:id"></Route>
+          <Route path="/admin/login"></Route>
 
           <Route path="/sheets">
             <AllSheets />
@@ -26,7 +28,9 @@ function App() {
           <Route path="/reviews">
             <AllReviews />
           </Route>
-          <Route path="/admin"></Route>
+          <Route path="/admin">
+            <AdminHome />
+          </Route>
 
           <Route path="/">
             <HomePage />
