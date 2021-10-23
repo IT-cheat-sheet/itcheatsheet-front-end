a56bb6c8793389666f8ff0a5d31f62485753f228
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-24 00:14:20 +0700

    [EDIT] Update SearchBox

--- src/core/components/searchBox.js
+++ src/core/components/searchBox.js
@@ -35,7 +35,8 @@ export default function SearchBox({ page, options, onFilter, onSearch }) {
           }}
           className={classNames("dropdown w-48 py-3 px-4 body-base cursor-pointer hover:text-white rounded-t-lg",
         {'bg-lightblue-lighter hover:bg-blue-form text-blue-body' : page === "review"},
-        {'bg-violet-bubbleHover hover:bg-violet-pill text-violet-bubbleText' : page === "sheet"})}>None</div>
+        {'bg-violet-bubbleHover hover:bg-violet-pill text-violet-bubbleText' : page === "sheet"},
+        {'bg-lightblue-button hover:bg-lightblue-hover text-lightblue-hover' : page === "admin"})}>None</div>
         {options.map((option, index) => (
           <div
           key={index}
@@ -46,7 +47,8 @@ export default function SearchBox({ page, options, onFilter, onSearch }) {
           className={classNames("dropdown w-48 py-3 px-4 body-base cursor-pointer  hover:text-white",
           {'rounded-b-lg' : index === options.length - 1},
           {'bg-lightblue-lighter hover:bg-blue-form text-blue-body' : page === "review"},
-          {'bg-violet-bubbleHover hover:bg-violet-pill text-violet-bubbleText' : page === "sheet"}
+          {'bg-violet-bubbleHover hover:bg-violet-pill text-violet-bubbleText' : page === "sheet"},
+          {'bg-lightblue-button hover:bg-lightblue-hover text-lightblue-hover' : page === "admin"}
           )
           }>{option.key}</div>
         ))}
@@ -56,24 +58,28 @@ export default function SearchBox({ page, options, onFilter, onSearch }) {
 
   return (
     <div>
-      <div className={classNames("hidden md:grid grid-cols-12 gap-5 px-6 py-4 rounded-lg body-base",
+      <div className={classNames("hidden md:grid grid-cols-12 gap-5 rounded-lg body-base",
       {"bg-lightblue-bg" : page === "review",},
-      {"bg-violet-bubbleHover" : page === "sheet"}
+      {"bg-violet-bubbleHover" : page === "sheet"},
+      {"px-6 py-4" : page !== 'admin'}
       )}>
-        <div className="col-span-2 relative dropdown" onClick={() => setToggle(!toggle)}>
-          <Button color={page === "review" ? "blue" : "purple"} size="sm" children="SORT BY" />
+        <div className={`${page === 'admin' ? 'col-span-3' : 'col-span-2'} relative dropdown`}>
+          <Button color={page === "review" ? "blue" : page === "sheet" ? "purple" : "lightblue"} size="sm" children="SORT BY" onClick={() => setToggle(!toggle)} />
           {toggle ? <Dropdown /> : <></>}
         </div>
-        <div className="col-span-8"><input className="w-full h-full rounded-lg px-4 text-purple-hover focus:outline-none" onChange={(e) => setSearchWord(e.target.value)} /></div>
-        <div className="col-span-2" onClick={() => onSearch(searchWord)}><Button color={page === "review" ? "blue" : "purple"} size="sm" children={<span className="material-icons text-4xl">search</span>} /></div>
+        <div className={page === 'admin' ? 'col-span-6 col-start-5' : 'col-span-8'}><input className={classNames("w-full h-full rounded-lg px-4 focus:outline-none",
+        {"text-purple-hover": page === "sheet"},
+        {"text-blue-body": page === "review"},
+        {"text-gray-mailbox bg-gray-form bg-opacity-50": page === "admin"})} onChange={(e) => setSearchWord(e.target.value)} /></div>
+        <div className="col-span-2"><Button color={page === "sheet" ? "purple" : "blue"} size="sm" children={<span className="material-icons text-4xl">search</span>} onClick={() => onSearch(searchWord)} /></div>
       </div>
       <div className="grid md:hidden grid-cols-12 gap-x-3 body-base">
         <div className="col-span-8"><input className={classNames("w-full h-full rounded-lg px-4 focus:outline-none",
         {"text-purple-hover" : page === "sheet"},
         {"text-blue-body bg-lightblue-bg" : page === "review"})} onChange={(e) => setSearchWord(e.target.value)} /></div>
-        <div className="col-span-2" onClick={() => onSearch(searchWord)}><Button color={page === "review" ? "blue" : "purple"} size="sm" children={<span className="material-icons text-3xl">search</span>} /></div>
-        <div className="col-span-2 relative dropdown" onClick={() => setToggle(!toggle)}>
-          <Button color={page === "review" ? "blue" : "purple"} size="sm" children={<span className="material-icons text-3xl">filter_list</span>} />
+        <div className="col-span-2"><Button color={page === "review" ? "blue" : "purple"} size="sm" children={<span className="material-icons text-3xl">search</span>} onClick={() => onSearch(searchWord)} /></div>
+        <div className="col-span-2 relative dropdown">
+          <Button color={page === "review" ? "blue" : "purple"} size="sm" children={<span className="material-icons text-3xl">filter_list</span>} onClick={() => setToggle(!toggle)} />
           {toggle ? <Dropdown /> : <></>}
         </div>
       </div>
