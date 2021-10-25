d0d03f40520e05d6c45e646d0d8b84c7655155b2
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-25 17:35:47 +0700

    [EDIT] Use MobX in All Sheet Page

--- src/core/service/getSheet.js
+++ src/core/service/getSheet.js
@@ -8,6 +8,14 @@ export const getSemester = () => {
   return axios.get("/semester/getall");
 };
 
+export const getTopic = () => {
+  return axios.get("http://localhost:3000/topic/getAll");
+}
+
+export const getReview = (page, pageSize, searchWord, filter) => {
+  return axios.get(`http://localhost:3000/review/getAll?page=${page - 1}&pageSize=${pageSize}&searchWord=${searchWord}&sortTopic=${filter}&sortDesc=true`);
+};
+
 export const getReport = (filter, searchWord, readPage, token) => {
   return axios.get(`http://localhost:3000/report/getAll?sortBy=${filter}&search=${searchWord}&readStatus=${readPage ? 1 : 0}`,
   {
