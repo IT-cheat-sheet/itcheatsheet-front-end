dc2baefc670f0bd8b001dfc33e93c76fb764812a
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-10-31 20:16:54 +0700

    [EDIT] Use env Variable in Services

--- src/core/service/deleteSheet.js
+++ src/core/service/deleteSheet.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const endOfHost = window.location.origin.lastIndexOf(':') <= 5 ? window.location.origin.length : window.location.origin.lastIndexOf(':');
+const host = process.env.REACT_APP_BE_HOST === 'CLIENT' ? window.location.origin.slice(0, endOfHost) : process.env.REACT_APP_BE_HOST;
+const port = process.env.REACT_APP_BE_PORT === 'CLIENT' ? (endOfHost !== window.location.origin.length ? ":" + window.location.origin.slice(endOfHost + 1, window.location.origin.length) : "" ) : ":" + process.env.REACT_APP_BE_PORT;
+
 export const deleteSheet = (id, token) => {
-  return axios.delete(`http://localhost:3000/summarypost/delete/${id}`,{
+  return axios.delete(`${host}${port}/summarypost/delete/${id}`,{
     headers: {
       Authorization: `Bearer ${token}`
     }
@@ -9,7 +13,7 @@ export const deleteSheet = (id, token) => {
 }
 
 export const deleteReview = (id, token) => {
-  return axios.delete(`http://localhost:3000/review/delete/${id}`, {
+  return axios.delete(`${host}${port}/review/delete/${id}`, {
     headers: {
       Authorization: `Bearer ${token}`
     }
--- src/core/service/getSheet.js
+++ src/core/service/getSheet.js
@@ -1,27 +1,31 @@
 import axios from "axios";
 
+const endOfHost = window.location.origin.lastIndexOf(':') <= 5 ? window.location.origin.length : window.location.origin.lastIndexOf(':');
+const host = process.env.REACT_APP_BE_HOST === 'CLIENT' ? window.location.origin.slice(0, endOfHost) : process.env.REACT_APP_BE_HOST;
+const port = process.env.REACT_APP_BE_PORT === 'CLIENT' ? (endOfHost !== window.location.origin.length ? ":" + window.location.origin.slice(endOfHost + 1, window.location.origin.length) : "" ) : ":" + process.env.REACT_APP_BE_PORT;
+
 export const getSubject = () => {
-  return axios.get("/subject/getall");
+  return axios.get(`${host}${port}/subject/getall`);
 };
 
 export const getSemester = () => {
-  return axios.get("/semester/getall");
+  return axios.get(`${host}${port}/semester/getall`);
 };
 
 export const getTopic = () => {
-  return axios.get("http://localhost:3000/topic/getAll");
+  return axios.get(`${host}${port}/topic/getAll`);
 }
 
 export const getReview = (page, pageSize, searchWord, filter) => {
-  return axios.get(`http://localhost:3000/review/getAll?page=${page - 1}&pageSize=${pageSize}&searchWord=${searchWord}&sortTopic=${filter}&sortDesc=true`);
+  return axios.get(`${host}${port}/review/getAll?page=${page - 1}&pageSize=${pageSize}&searchWord=${searchWord}&sortTopic=${filter}&sortDesc=true`);
 };
 
 export const getSheet = (page, pageSize, searchWord, filter) => {
-  return axios.get(`http://localhost:3000/summarypost/getAll?pageNumber=${page}&pageSize=${pageSize}&search=${searchWord}&semesterFilter=${filter}&sortType=DESC`);
+  return axios.get(`${host}${port}/summarypost/getAll?pageNumber=${page}&pageSize=${pageSize}&search=${searchWord}&semesterFilter=${filter}&sortType=DESC`);
 };
 
 export const getReport = (filter, searchWord, readPage, token) => {
-  return axios.get(`http://localhost:3000/report/getAll?sortBy=${filter}&search=${searchWord}&readStatus=${readPage ? 1 : 0}`,
+  return axios.get(`${host}${port}/report/getAll?sortBy=${filter}&search=${searchWord}&readStatus=${readPage ? 1 : 0}`,
   {
     headers: {
       "Authorization": `Bearer ${token}`
@@ -30,34 +34,34 @@ export const getReport = (filter, searchWord, readPage, token) => {
 };
 
 export const getReviewImage = (id) => {
-  return axios.get(`http://localhost:3000/review/image/${id}`,
+  return axios.get(`${host}${port}/review/image/${id}`,
   {
     responseType: "blob"
   });
 }
 
 export const getSpecificSheet = (id) => {
-  return axios.get(`http://localhost:3000/summarypost/get/${id}`);
+  return axios.get(`${host}${port}/summarypost/get/${id}`);
 }
 
 export const getSpecificPdf = (id) => {
-  return axios.get(`http://localhost:3000/summarypost/getFile/${id}`,{
+  return axios.get(`${host}${port}/summarypost/getFile/${id}`,{
     responseType: 'blob'
   })
 }
 
 export const getSpecificReview = (id) => {
-  return axios.get(`http://localhost:3000/review/get/${id}`);
+  return axios.get(`${host}${port}/review/get/${id}`);
 }
 
 export const getRecommendedReviews = () => {
-  return axios.get("http://localhost:3000/review/random");
+  return axios.get(`${host}${port}/review/random`);
 }
 
 export const getSuggestedSheet = (sem) => {
-  return axios.get(`http://localhost:3000/summarypost/hotSheet/${sem}`);
+  return axios.get(`${host}${port}/summarypost/hotSheet/${sem}`);
 }
 
 export const getSuggestedReview = (rand) => {
-  return axios.get(`http://localhost:3000/review/hotReview/${rand}`);
+  return axios.get(`${host}${port}/review/hotReview/${rand}`);
 }
