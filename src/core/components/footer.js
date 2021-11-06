384d7e34162f9d49128cc711e673de35d40916d7
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-11-07 02:49:35 +0700

    [EDIT] Remove Email from Footer

--- src/core/components/footer.js
+++ src/core/components/footer.js
@@ -2,15 +2,15 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <div className=" static bottom-0 grid w-full bg-gray-100 py-6 text-xs tracking-tight  sm:text-sm sm:tracking-wide justify-self-center bg-gray-footer text-gray-subheader">
-            <div className="flex place-self-center " >
-                <div className="flex items-center " >
-                    <div className=" flex place-self-center  ">
-                        <span className="material-icons font-light  ">
+        <div className=" static bottom-0 grid w-full bg-gray-100 py-6 text-xs tracking-tight sm:text-sm sm:tracking-wide justify-self-center bg-gray-footer text-gray-subheader">
+            <div className="flex place-self-center">
+                <div className="flex items-center">
+                    <div className="flex">
+                        <span className="material-icons text-xl">
                             copyright
                         </span>
                     </div>
-                    <p className="mt-1 pl-1">Copyright 2021  |  SIT KMUTT | admin.report@gmail.com  </p>
+                    <p className="pl-1">Copyright 2021  |  IT25 KMUTT</p>
                 </div>
             </div>
             {/* <div className="flex place-self-center ">
