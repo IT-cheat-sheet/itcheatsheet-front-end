b80ff485201a1ebb82fcad11eaedd8b6208cb469
Wlthr <napat.watt@mail.kmutt.ac.th>
2021-11-13 23:47:59 +0700

    [EDIT] Responsive Pagination

--- src/core/components/pagination.js
+++ src/core/components/pagination.js
@@ -33,21 +33,21 @@ export default function Pagination({ page, current, total, url }) {
 
   return (
     <div>
-      <div className="body-base w-full flex justify-center">
-        <div className="hidden md:block text-gray-header">
+      <div className="text-lg md:text-2xl w-full flex justify-center">
+        <div className="hidden md:flex text-gray-header w-full justify-around">
           <button className={classNames("font-bold mx-4", {"text-gray-disabled cursor-default": current === 1})} disabled={current === 1} onClick={() => pageHop(1)}>&lt;&lt;</button>
           <button className={classNames("font-bold mx-4", {"text-gray-disabled cursor-default": current === 1})} disabled={current === 1} onClick={() => pageHop(current - 1)}>&lt;</button>
           <LoopPage totalPages={10}/>
           <button className={classNames("font-bold mx-4", {"text-gray-disabled cursor-default": current === total})} disabled={current === total} onClick={() => pageHop(current + 1)}>&gt;</button>
           <button className={classNames("font-bold mx-4", {"text-gray-disabled cursor-default": current === total})} disabled={current === total} onClick={() => pageHop(total)}>&gt;&gt;</button>
         </div>
-        <div className="md:hidden text-gray-header">
-          <button className={classNames("font-bold mx-3 p-3 rounded-lg",
+        <div className="md:hidden text-gray-header flex w-full justify-around">
+          <button className={classNames("font-bold p-3 rounded-lg",
           {"cursor-default": current === 1},
           {"bg-purple-form bg-opacity-40 text-violet-sheet" : page === "sheet"},
           {"bg-blue-button text-white" : page === "review"})} disabled={current === 1} onClick={() => pageHop(current - 1)}><span className="material-icons font-bold block">chevron_left</span></button>
           <LoopPage totalPages={4}/>
-          <button className={classNames("font-bold mx-3 p-3 rounded-lg",
+          <button className={classNames("font-bold p-3 rounded-lg",
           {"cursor-default": current === 1},
           {"bg-purple-form bg-opacity-40 text-violet-sheet" : page === "sheet"},
           {"bg-blue-button text-white" : page === "review"})} disabled={current === total} onClick={() => pageHop(current + 1)}><span className="material-icons font-bold block">chevron_right</span></button>
