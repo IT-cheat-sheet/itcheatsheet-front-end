4d78c526ec9c7adc49b873347836b48b35ca2f39
Pathinya <gift_01210@hotmail.com>
2021-11-20 11:38:48 +0700

    [EDIT] animation bug fixed

--- src/pages/preview-sheet/preview_sheet_page.jsx
+++ src/pages/preview-sheet/preview_sheet_page.jsx
@@ -32,26 +32,26 @@ export default function PreviewSheet() {
       {() => (
         <div className="min-h-screen flex flex-col justify-between">
           <div>
-          <Navbar />
-          {context.isLoad &&
-            <div className={`px-4 pb-12 md:px-16 lg:px-20 xl:px-44 bg-violet-bg pt-24 lg:pt-0 md:bg-white ${context.file ? "md:grid-cols-12 md:grid md:gap-7" : ""}`}>
-              <div className="col-span-5">
-                <div className="md:hidden">
-                  <p className="text-xs text-purple-hover">{context.sheet.summaryPost.semester.semester}</p>
-                  <div className="mt-2">
-                    <h1 className="text-2xl font-bold text-violet-header mr-4">{context.sheet.summaryPost.summaryTitle}</h1>
-                    <div className="float-right -mt-8">
-                      <Kebab page="sheet" postId={context.sheet.summaryPost.summaryPostId} />
+            <Navbar />
+            {context.isLoad &&
+              <div className={`px-4 pb-12 md:px-16 lg:px-20 xl:px-44 bg-violet-bg pt-24 lg:pt-0 md:bg-white ${context.file ? "md:grid-cols-12 md:grid md:gap-7" : ""}`}>
+                <div className="col-span-5">
+                  <div className="md:hidden">
+                    <p className="text-xs text-purple-hover">{context.sheet.summaryPost.semester.semester}</p>
+                    <div className="mt-2">
+                      <h1 className="text-2xl font-bold text-violet-header mr-4">{context.sheet.summaryPost.summaryTitle}</h1>
+                      <div className="float-right -mt-8">
+                        <Kebab page="sheet" postId={context.sheet.summaryPost.summaryPostId} />
+                      </div>
+                    </div>
+                    <div className="flex flex-col mt-1 mb-5 space-y-2">
+                      <p className="px-2 py-0.5 text-xs rounded-3xl bg-violet-pill text-white whitespace-nowrap w-max">{context.sheet.summaryPost.posterName}</p>
+                      <p className="text-purple-hover text-xs truncate">{context.sheet.summaryPost.subjects.subjectId + ' ' + _.startCase(context.sheet.summaryPost.subjects.subjectName.toLowerCase()).replace(/[i]{2,}/i, match => match.toUpperCase())}</p>
                     </div>
                   </div>
-                  <div className="flex flex-col mt-1 mb-5 space-y-2">
-                    <p className="px-2 py-0.5 text-xs rounded-3xl bg-violet-pill text-white whitespace-nowrap w-max">{context.sheet.summaryPost.posterName}</p>
-                    <p className="text-purple-hover text-xs truncate">{context.sheet.summaryPost.subjects.subjectId + ' ' + _.startCase(context.sheet.summaryPost.subjects.subjectName.toLowerCase()).replace(/[i]{2,}/i, match => match.toUpperCase())}</p>
-                  </div>
-                </div>
-                <div className={`rounded-button overflow-hidden ${context.file && 'mt-5'} ${isPdfLoad && 'shadow-halo'}`}>
-                  {
-                    context.file &&
+                  <div className={`rounded-button overflow-hidden ${context.file && 'mt-5'} ${isPdfLoad && 'shadow-halo'}`}>
+                    {
+                      context.file &&
                       <a id="pdfOpen" href={`${host}/summarypost/getFile/${params.id}`} target="_blank" rel="noreferrer">
                         <div>
                           <Document file={context.file} onLoadSuccess={() => setIsPdfLoad(true)} loading={
@@ -61,78 +61,80 @@ export default function PreviewSheet() {
                             </div>
                           }>
                             <Page pageNumber={1} renderTextLayer={false} renderAnnotationLayer={false} loading={
-                            <div className="text-violet-hover text-center pt-20">
-                              <span className="material-icons-round md:text-iconLoad text-3xl spin">hourglass_top</span>
-                              <div>Loading... Please Wait</div>
-                            </div>
-                          }/>
+                              <div className="text-violet-hover text-center pt-20">
+                                <span className="material-icons-round md:text-iconLoad text-3xl spin">hourglass_top</span>
+                                <div>Loading... Please Wait</div>
+                              </div>
+                            } />
                           </Document>
                         </div>
                       </a>
-                  }
-                </div>
-                {context.file && isPdfLoad && (<div className="w-full text-center mt-2 text-purple-hover text-sm md:text-2xl">Click PDF to View File</div>)}
+                    }
+                  </div>
+                  {context.file && isPdfLoad && (<div className="w-full text-center mt-2 text-purple-hover text-sm md:text-2xl">Click PDF to View File</div>)}
 
-              </div>
-              <div className="mt-5 xl:mt-7 md:col-span-7">
-                <div className="hidden md:block">
-                  <div className="flex">
-                    <h2 className="header-secondary text-violet-header">{context.sheet.summaryPost.summaryTitle}</h2>
-                    <p className="ml-3 text-purple-hover mt-9">{context.sheet.summaryPost.semester.semester}</p>
-                  </div>
-                  <div className="flex my-2">
-                    <p className="px-6 rounded-3xl bg-violet-pill text-white whitespace-nowrap">{context.sheet.summaryPost.posterName}</p>
-                    <p className="ml-2.5 text-purple-hover truncate">{context.sheet.summaryPost.subjects.subjectId + ': ' + _.startCase(context.sheet.summaryPost.subjects.subjectName.toLowerCase()).replace(/[i]{2,}/i, match => match.toUpperCase())}</p>
-                  </div>
                 </div>
-                <div className="text-xs md:mt-5 md:text-2xl">
-                  <div className="bg-violet-bg md:pt-5 pb-6 md:pb-12">
-                    <div className="hidden md:flex md:justify-end md:mr-6">
-                      <Kebab page="sheet" postId={context.sheet.summaryPost.summaryPostId} />
+                <div className="mt-5 xl:mt-7 md:col-span-7">
+                  <div className="hidden md:block">
+                    <div className="flex">
+                      <h2 className="header-secondary text-violet-header">{context.sheet.summaryPost.summaryTitle}</h2>
+                      <p className="ml-3 text-purple-hover mt-9">{context.sheet.summaryPost.semester.semester}</p>
                     </div>
-                    <div className="md:mx-11">
-                      <p ref={ref} className={`text-purple-hover ${toggleSeeMore ? "md:overflow-y-scroll" : "line-clamp-6"} md:max-h-72 innerTrack px-3`}>
-                        {context.sheet.summaryPost.summaryContent}
-                      </p>
-                      {
-                        !toggleSeeMore && context.exceed &&
-                        <div className="text-violet-link hover:text-violet-admin cursor-pointer px-3" onClick={() => setToggleSeeMore(true)}>see more</div>
-                      }
-                      {
-                        context.sheet.summaryPost.linkAttachment ?
-                          <a href={context.sheet.summaryPost.linkAttachment} className="text-violet-link mt-5 md:mt-10 flex items-center line-clamp-1 break-all" target="_blank" rel="noreferrer">
-                            <span className="material-icons text-base md:text-3xl transform -rotate-45">link</span>
-                            <span className="ml-1 md:ml-2 underline">{context.sheet.summaryPost.linkAttachment}</span>
+                    <div className="flex my-2">
+                      <p className="px-6 rounded-3xl bg-violet-pill text-white whitespace-nowrap">{context.sheet.summaryPost.posterName}</p>
+                      <p className="ml-2.5 text-purple-hover truncate">{context.sheet.summaryPost.subjects.subjectId + ': ' + _.startCase(context.sheet.summaryPost.subjects.subjectName.toLowerCase()).replace(/[i]{2,}/i, match => match.toUpperCase())}</p>
+                    </div>
+                  </div>
+                  <div className="text-xs md:mt-5 md:text-2xl">
+                    <div className="bg-violet-bg md:pt-5 pb-6 md:pb-12">
+                      <div className="hidden md:flex md:justify-end md:mr-6">
+                        <Kebab page="sheet" postId={context.sheet.summaryPost.summaryPostId} />
+                      </div>
+                      <div className="md:mx-11">
+                        <p ref={ref} className={`text-purple-hover ${toggleSeeMore ? "md:overflow-y-scroll" : "line-clamp-6"} md:max-h-72 innerTrack px-3`}>
+                          {context.sheet.summaryPost.summaryContent}
+                        </p>
+                        {
+                          !toggleSeeMore && context.exceed &&
+                          <div className="text-violet-link hover:text-violet-admin cursor-pointer px-3" onClick={() => setToggleSeeMore(true)}>see more</div>
+                        }
+                        {
+                          context.sheet.summaryPost.linkAttachment ?
+                            <a href={context.sheet.summaryPost.linkAttachment} className="text-violet-link mt-5 md:mt-10 flex items-center line-clamp-1 break-all" target="_blank" rel="noreferrer">
+                              <span className="material-icons text-base md:text-3xl transform -rotate-45">link</span>
+                              <span className="ml-1 md:ml-2 underline">{context.sheet.summaryPost.linkAttachment}</span>
+                            </a>
+                            : <></>
+                        }
+                      </div>
+                    </div>
+                    {
+                      context.file ?
+                        <div className="md:w-1/4 md:mb-12 text-center cursor-pointer mt-5 ml-auto text-white">
+                          <a href={context.file} download={context.sheet.summaryPost.subjects.subjectId + '-' + context.sheet.summaryPost.summaryPostId}>
+                            <Button color="purple" size="sm">Download</Button>
                           </a>
-                          : <></>
-                      }
-                    </div>
+                        </div>
+                        : <></>
+                    }
                   </div>
-                  {
-                    context.file ?
-                      <div className="md:w-1/4 md:mb-12 text-center cursor-pointer mt-5 ml-auto text-white">
-                        <a href={context.file} download={context.sheet.summaryPost.subjects.subjectId + '-' + context.sheet.summaryPost.summaryPostId}>
-                          <Button color="purple" size="sm">Download</Button>
-                        </a>
-                      </div>
-                      : <></>
-                  }
                 </div>
               </div>
-            </div>
-          }
-          {context.loading && (
-                  <div className="absolute table bg-black opacity-60 w-full h-full">
-                    <div className="table-cell align-middle">
-                      <div className="flex justify-center">
-                        <img
-                          src="https://www.isabelhealthcare.com/assets/post_isabel/loading4-2fa514fd64ec8a05a04f3fc45b438f7e.gif"
-                          alt="loading animation"
-                          className="w-20 h-20" />
-                      </div>
-                    </div>
+            }
+            {context.loading && (
+              <div className="fixed table bg-black opacity-60 w-full h-full top-0 z-60">
+                <div className="table-cell align-middle">
+                  <div className="flex justify-center">
+                    <img
+                      src="https://www.isabelhealthcare.com/assets/post_isabel/loading4-2fa514fd64ec8a05a04f3fc45b438f7e.gif"
+                      alt="loading animation"
+                      className="w-20 h-20" />
                   </div>
-                )}
+                  <br />
+                  <p className="text-white text-center">Loading sheet. Please wait.</p>
+                </div>
+              </div>
+            )}
           </div>
           <Footer />
         </div>
